db82e8f43ba625e469d35dad9bbbd25b34d4d000
Mohammad Toosi <m_dd@rogers.com>
2021-06-12 19:19:55 +0430

    updated packages and fixed development setup

--- packages/react-mmd-persian-datepicker/rollup.config.ts
+++ packages/react-mmd-persian-datepicker/rollup.config.ts
@@ -39,9 +39,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
+    typescript(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
---
+++ update-packages.js
@@ -0,0 +1,25 @@
+const ncu = require("npm-check-updates");
+const path = require("path");
+
+const packageName = "package.json";
+
+const packages = [
+  path.resolve("package.json"),
+  path.resolve("packages", "mmd-persian-datepicker", packageName),
+  path.resolve("packages", "react-mmd-persian-datepicker", packageName),
+];
+
+const run = async () => {
+  for (const package of packages) {
+    const upgraded = await ncu.run({
+      packageFile: package,
+      upgrade: true,
+    });
+
+    console.log(`upgraded for ${package}: `);
+    console.log(upgraded);
+    console.log("-".repeat(20));
+  }
+};
+
+run();
